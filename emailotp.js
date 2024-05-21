69d97f8ff40f5a8d18fa3cba7cff9e50a6d834f0
Wil518340 <137013950+Wil518340@users.noreply.github.com>
2024-05-21 17:56:33 -0400

    prevent input disappear when refresh page

--- emailotp.js
+++ emailotp.js
@@ -1,10 +1,26 @@
+
+// window.onload function retrieves the saved values from Local Storage when the page loads and sets them to the input fields.
+window.onload = function() {
+    document.getElementById('email_id').value = localStorage.getItem('email_id') || '';
+    document.getElementById('full_name').value = localStorage.getItem('full_name') || '';
+};
+
+// Save input values to Local Storage
+function saveInputValue(id) {
+    const value = document.getElementById(id).value;
+    localStorage.setItem(id, value);
+}
+
 function sendOTP() {
+    const [inputValue, setInputValue] = useState('');
+
     let otp_val = Math.floor(1000 + Math.random() * 9000);
     var params = {
-        from_name: document.getElementById("Full Name").value, 
+        from_name: document.getElementById("full_name").value, 
         email_id: document.getElementById("email_id").value, 
         verify: otp_val
     };
+
     
     //  emailjs.send('service ID', 'template ID', parameters that match the placeholders in your email template)
     emailjs.send('service_v7xm339', 'template_9kr8sum', params) 
@@ -21,3 +37,5 @@ function sendOTP() {
     })
     
 }
+
+
